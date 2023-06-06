728049bef2c3431ec71215de4c0e4a8b8d22b760
xxh205 <hello@itcast.cn>
2023-06-06 22:05:25 +0100

    提交父工程和基础工程

--- xuecheng-plus-content/src/main/java/com/xuecheng/content/ContentApplication.java
+++ xuecheng-plus-content/src/main/java/com/xuecheng/content/ContentApplication.java
@@ -1,10 +1,12 @@
 package com.xuecheng.content;
 
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableSwagger2Doc
 @SpringBootApplication
 @MapperScan(basePackages = "com.xuecheng.content. mapper")
 public class ContentApplication {
--- xuecheng-plus-content/src/main/java/com/xuecheng/content/controller/CourseBaseInfoController.java
+++ xuecheng-plus-content/src/main/java/com/xuecheng/content/controller/CourseBaseInfoController.java
@@ -4,12 +4,15 @@ import com.xuecheng.PageParams;
 import com.xuecheng.PageResult;
 import com.xuecheng.content.dto.QueryCourseParamDto;
 import com.xuecheng.content.po.CourseBase;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value = "课程信息管理接口",tags = "课程信息管理接口")
 @RestController
 public class CourseBaseInfoController {
-
-    @RequestMapping("/course/list")
+    @ApiOperation("课程查询接口")
+    @PostMapping("/course/list")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamDto queryCourseParamDto){
         return null;
     }
